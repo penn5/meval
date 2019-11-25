3183b9fdf02bd95080fa03c5eb9376857bf9a9ce
Hackintosh 5 <hackintosh5@wildfyre.net>
2019-11-25 14:41:29 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="meval",
-    version="2.3",
+    version="2.4",
     author="Hackintosh 5",
     author_email="hackintoshfive@gmail.com",
     description="Performs async evaluations of strings",
